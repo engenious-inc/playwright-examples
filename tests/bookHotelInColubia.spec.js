74bf07c7d9e889e05511e55ba3a91bc731097ef2
b-baran-engenious <bbaran@engenious.io>
2024-03-01 14:08:49 +0200

    Tested ZeroStep

--- tests/bookHotelInColubia.spec.js
+++ tests/bookHotelInColubia.spec.js
@@ -1,36 +1,26 @@
 const { test, expect } = require('@playwright/test');
+import { ai } from '@zerostep/playwright'
 
-const dayjs = require('dayjs');
+// The ZeroStep code from their page
+test('search and verify the first organic search result', async ({ page }) => {
+  const searchTerm = 'software testing'
+  await page.goto('https://www.google.com')
+  await ai(`Search for '${searchTerm}'`, { page, test })
+  await ai('Hit enter', { page, test })
 
+  await page.waitForURL('https://www.google.com/search**')
+
+  const title = await ai(`What is the title of the first organic search result?`, { page, test })
+
+  console.log('First organic search result is: ', title)
+})
+// Refactored code from the 'Booking.com_chats_version' refactored to use ZeroStep library
 test('Search for a hotel in Columbia for the next 5 days', async ({ page }) => {
+  const aiArgs = { page, test }
   await page.goto('https://www.booking.com');
-  
-  // close the pop up
-  let crossPopUpToSigIn = page.locator(`[aria-label="Dismiss sign-in info."]`)
-  await expect(crossPopUpToSigIn).toBeVisible({timeout:20000})
-  await crossPopUpToSigIn.click()
-  // Enter the destination
-  await page.fill('[aria-label="Where are you going?"]', 'Columbia');
-
-  // Open calendar widget and set dates"
-  await page.click('[data-testid="searchbox-dates-container"]')
-  
-  // verify that calendar appear 
-  await expect( page.locator('[data-testid="datepicker-tabs"]')).toBeVisible()
-  
-  let [today,fiveDaysAfter] = getFormattedDates()
-  await page.locator(today).click();
-  await page.locator(fiveDaysAfter).click();
-  
-  await page.click('button[type="submit"]',{force:true})
+  await ai(`Wait for pop-up with text "Sign in to save 10% or more with a free Booking.com membership" to appear at the page `, aiArgs)
+  await ai(`"Please close the pop-up window."`, aiArgs)
+  await ai(`"Enter 'Columbia' into the 'Where I am going' field." `, aiArgs)
+  await ai(`Click on the field with text "Check-in Date"`, aiArgs)
+  await ai(`Wait for calendar to appear`, aiArgs)  
 });
-
-function getFormattedDates() {
-  const tomorrow = dayjs().add(1, 'day');
-  const inFiveDays = dayjs().add(6, 'day');
-
-  const formattedTomorrow = tomorrow.format('YYYY-MM-DD');
-  const formattedInFiveDays = inFiveDays.format('YYYY-MM-DD');
-
-  return [`[data-date="${formattedTomorrow}"]`, `[data-date="${formattedInFiveDays}"]`];
-}
