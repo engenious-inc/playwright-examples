11360f6e3383d2b97a53980411bec2568dee5acb
b-baran-engenious <bbaran@engenious.io>
2024-03-07 14:20:03 +0200

    Added test cases

--- tests/bookHotelInColubia.spec.js
+++ tests/bookHotelInColubia.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 const dayjs = require('dayjs');
 
-test('Search for a hotel in Columbia for the next 5 days', async ({ page }) => {
+test('Search for a hotel in Columbia for the next 5 days', async ({ page }) => {  
   await page.goto('https://www.booking.com');
   
   // close the pop up
@@ -24,6 +24,39 @@ test('Search for a hotel in Columbia for the next 5 days', async ({ page }) => {
   
   await page.click('button[type="submit"]',{force:true})
 });
+
+// All tests are for booking.com
+
+// Test case 1 : Verify that pop-up appears on first visit
+// Step 1: Open the browser and navigate to the booking.com 
+// Expected Result: Pop-up is displayed
+
+// Test case 2 : Verify that the "Sign in" pop-up is closed after clicking on the "X" button
+// Step 1: Open the browser and navigate to the booking.com
+// Step 2: Click on the "X" button
+// Expected Result: Pop-up is closed
+
+// Test case 2 : Verify that empty search results cause error message
+// Step 1: Open the browser and navigate to the booking.com and Close the pop-up
+// Step 2: Click on the search button
+// Expected Result: Error message is displayed
+
+// Test case 3 : Verify that calendar is displayed
+// Step 1: Open the browser and navigate to the booking.com and Close the pop-up
+// Step 2: Click on the "Check-out" input field
+// Expected Result: The calendar is displayed
+
+// Test case 4 : Verify that the calendar is closed after clicking outside the calendar
+// Step 1: Open the browser and navigate to the booking.com and Close the pop-up
+// Step 2: Click on the "Check-out" input field
+// Step 3: Click outside the calendar
+// Expected Result: The calendar is closed
+
+// Test case 5 : Verify that you are redirected to the Sign-in page after clicking on the "Sign in" button
+// Step 1: Open the browser and navigate to the booking.com and Close the pop-up
+// Step 2: Click on the "Sign in" button
+// Expected Result: The Sign-in page is displayed
+
 
 function getFormattedDates() {
   const tomorrow = dayjs().add(1, 'day');
