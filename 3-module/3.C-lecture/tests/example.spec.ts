3b005fa7f3cabe13846298008b2645effc9fb62f
Vasyl Shpak <vshpak@engenious.io>
2024-11-19 13:21:59 +0100

    update 3.C

--- 3-module/3.C-lecture/tests/example.spec.ts
+++ 3-module/3.C-lecture/tests/example.spec.ts
@@ -1,27 +1,38 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
 
-test('Verify "BMW M760" is visible on the "Our Cars" page', async ({
-  page,
-}) => {
-  await page.goto(
-    'https://course-user:Cou1dc4F@test.elitefleetgroup.engenious.io/',
-  );
-  await page.getByRole('link', { name: 'View All Cars' }).click();
-  await expect(
-    page.getByRole('button', { name: 'Car Image Car Image BMW M760' }).nth(1),
-  ).toBeVisible();
-});
+test.describe('Our Cars Page', () => {
+  //? Helpers
+  const navigateToOurCarsPage = async (page: Page): Promise<void> => {
+    await page.goto(
+      'https://course-user:Cou1dc4F@test.elitefleetgroup.engenious.io/',
+    );
+    await page.getByRole('link', { name: 'View All Cars' }).click();
+  };
+  const getBMWM760Button = (page: Page): Locator =>
+    page.getByRole('button', { name: 'Car Image Car Image BMW M760' }).nth(1);
+  const getBMWM760DetailsText = (page: Page): Locator =>
+    page.getByText('BMW M760');
+  const getBMWM760PriceText = (page: Page): Locator =>
+    page.getByText('BMW M760$450/dayBook Now');
+  const getBookNowButton = (page: Page): Locator =>
+    page.getByText('Book Now').nth(2);
 
-test('Navigate to and Verify the "BMW M760" Details Page', async ({ page }) => {
-  await page.goto(
-    'https://course-user:Cou1dc4F@test.elitefleetgroup.engenious.io/',
-  );
-  await page.getByRole('link', { name: 'View All Cars' }).click();
+  test.beforeEach(async ({ page }) => {
+    await navigateToOurCarsPage(page);
+  });
 
-  await expect(
-    page.getByRole('button', { name: 'Car Image Car Image BMW M760' }).nth(1),
-  ).toBeVisible();
-  await expect(page.getByText('BMW M760')).toBeVisible();
-  await expect(page.getByText('BMW M760$450/dayBook Now')).toBeVisible();
-  await expect(page.getByText('Book Now').nth(2)).toBeVisible();
+  test('Verify "BMW M760" is visible on the "Our Cars" page', async ({
+    page,
+  }) => {
+    await expect(getBMWM760Button(page)).toBeVisible();
+  });
+
+  test('Navigate to and Verify the "BMW M760" Details Page', async ({
+    page,
+  }) => {
+    await expect(getBMWM760Button(page)).toBeVisible();
+    await expect(getBMWM760DetailsText(page)).toBeVisible();
+    await expect(getBMWM760PriceText(page)).toBeVisible();
+    await expect(getBookNowButton(page)).toBeVisible();
+  });
 });
