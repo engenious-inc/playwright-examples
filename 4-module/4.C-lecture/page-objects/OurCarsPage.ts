d802ce491eacc22e55644d5132668e9183d375ed
Vasyl Shpak <vshpak@engenious.io>
2024-11-27 12:16:40 +0100

    add lecture 4.B, 4.C

---
+++ 4-module/4.C-lecture/page-objects/OurCarsPage.ts
@@ -0,0 +1,30 @@
+import { Locator } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class OurCarsPage extends BasePage {
+  async navigateToOurCarsPage(): Promise<void> {
+    //https://course-user:Cou1dc4F@test.elitefleetgroup.engenious.io/
+    await this.navigateTo('/our-cars');
+  }
+
+  async getCarButton(carName: string): Promise<Locator> {
+    return this.page
+      .getByRole('button', { name: `Car Image Car Image ${carName}` })
+      .nth(1);
+  }
+  getCarDetailsText(carName: string): Locator {
+    return this.page.getByText(carName);
+  }
+
+  getCarPriceText(carName: string, price: string): Locator {
+    return this.page.getByText(`${carName}${price}Book Now`);
+  }
+
+  getBookNowButton(): Locator {
+    return this.page.getByText('Book Now').nth(2);
+  }
+
+  async isCarButtonVisible(carName: string): Promise<boolean> {
+    return (await this.getCarButton(carName)).isVisible();
+  }
+}
