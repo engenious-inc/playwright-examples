5a418578b7dd8a61efb1f6bba2bab444c8602a25
Vasyl Shpak <vshpak@engenious.io>
2025-01-31 13:48:51 +0100

    update 5.E

--- 5-module/5.E-lecture/tests/booking-tests.spec.ts
+++ 5-module/5.E-lecture/tests/booking-tests.spec.ts
@@ -2,6 +2,11 @@ import { expect } from '@playwright/test';
 import { carData } from '../data/carData';
 import { test } from '../fixtures/global';
 
+//  { model: 'Sedan', price: 100, carType: 'Economy' },
+//   { model: 'SUV', price: 150, carType: 'Luxury' },
+//   { model: 'Convertible', price: 200, carType: 'Premium' },
+//   { model: 'Truck', price: 180, carType: 'Heavy Duty' },
+
 test.describe('Car Booking Tests', () => {
   for (const { model, price } of carData) {
     test(`should book a ${model} model with price $${price}`, async ({
