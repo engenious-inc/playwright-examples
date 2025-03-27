84be47450078c73cf6b087d50705ae928ca5e2ac
Vasyl Shpak <vshpak@engenious.io>
2025-03-27 18:23:04 +0100

    6.E

--- 6-module/6.E-lecture/fixtures/global.ts
+++ 6-module/6.E-lecture/fixtures/global.ts
@@ -3,12 +3,14 @@ import { LoginPage } from '../page-objects/LoginPage';
 import { OurCarsPage } from '../page-objects/OurCarsPage';
 import { NavigationPage } from '../page-objects/NavigationPage';
 import { BookingCar } from '../page-objects/BookingCar';
+import { AboutUsPage } from '../page-objects/AboutUsPage';
 
 type Fixtures = {
   loginPage: LoginPage;
   ourCarsPage: OurCarsPage;
   navigationPage: NavigationPage;
   bookingCarPage: BookingCar;
+  aboutUsPage: AboutUsPage;
 };
 
 const test = baseTest.extend<Fixtures>({
@@ -27,6 +29,10 @@ const test = baseTest.extend<Fixtures>({
   bookingCarPage: async ({ page }, use) => {
     const bookingCarPage = new BookingCar(page);
     await use(bookingCarPage);
+  },
+  aboutUsPage: async ({ page }, use) => {
+      const aboutUsPage = new AboutUsPage(page);
+      await use(aboutUsPage);
   },
 });
 
