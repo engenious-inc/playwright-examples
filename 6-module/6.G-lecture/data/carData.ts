04dc4cb26c5ac4010f5f7980c84c8897158db3ec
Vasyl Shpak <vshpak@engenious.io>
2025-04-02 19:37:35 +0200

    add 6.G

---
+++ 6-module/6.G-lecture/data/carData.ts
@@ -0,0 +1,6 @@
+export const carData = [
+  { model: 'Sedan', price: 100, carType: 'Economy' },
+  { model: 'SUV', price: 150, carType: 'Luxury' },
+  { model: 'Convertible', price: 200, carType: 'Premium' },
+  { model: 'Truck', price: 180, carType: 'Heavy Duty' },
+];
